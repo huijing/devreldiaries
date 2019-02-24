eba40625b67da31842a2b608a2ea582e5c1331f0
Chen Hui Jing <huijing.chen@vonage.com>
2019-02-24 12:30:37 +0800

    Add rss and sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,22 @@
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRss)
   eleventyConfig.addNunjucksFilter('dateFormat', date => {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
     return date.toLocaleDateString('en-SG', options)
   })
 
+  eleventyConfig.addPassthroughCopy('src/views/googlefa6ce6b0c8472bb7.html');
+
   return {
+    templateFormats: [
+      "md",
+      "njk"
+    ],
+    passthroughFileCopy: true,
     dir: {
+      data: '../_data',
       input: 'src/views',
       output: 'dist'
     }
